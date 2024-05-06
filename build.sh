f66b723e07af08734cf218285929e81ba5b76d62
khanhdt <khanhdt@smartosc.com>
2024-05-06 13:21:16 +0700

    fix: fix script build osmosis

--- build.sh
+++ build.sh
@@ -15,5 +15,7 @@ cd ${SCRIPT_DIR}/relayer && docker compose build
 
 # Build osmosis
 git submodule update --remote --merge
+chmod +x ${SCRIPT_DIR}/setup_osmosis.sh
+bash ${SCRIPT_DIR}/setup_osmosis.sh
 cd ${SCRIPT_DIR}/chains/osmosis && make localnet-init
 
--- setup_osmosis.sh
+++ setup_osmosis.sh
@@ -1,4 +1,1 @@
 #!/usr/bin/env bash
-
-cp osmosis.patch ./chains/osmosis/osmosis.patch
-cd ./chains/osmosis && git apply osmosis.patch
