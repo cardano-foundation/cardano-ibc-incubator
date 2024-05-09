37709c54cd26d1216b1a0e2ef0858cab995be3ad
khanhdt <khanhdt@smartosc.com>
2024-05-09 11:27:49 +0700

    feat: build message directly in send function

--- relayer/constant/constant.go
+++ relayer/constant/constant.go
@@ -6,4 +6,17 @@ const (
 	EpochPeriod          = 5
 	ClientTrustingPeriod = EpochPeriod * 24 * time.Hour
 	OgmiosEndpoint       = "OGMIOS_ENDPOINT"
+
+	MsgCreateClient        = "/ibc.core.client.v1.MsgCreateClient"
+	MsgUpdateClient        = "/ibc.core.client.v1.MsgUpdateClient"
+	MsgConnectionOpenInit  = "/ibc.core.connection.v1.MsgConnectionOpenInit"
+	MsgConnectionOpenAck   = "/ibc.core.connection.v1.MsgConnectionOpenAck"
+	MsgChannelOpenInit     = "/ibc.core.channel.v1.MsgChannelOpenInit"
+	MsgChannelOpenAck      = "/ibc.core.channel.v1.MsgChannelOpenAck"
+	MsgApplicationTransfer = "/ibc.applications.transfer.v1.MsgTransfer"
+	MsgRecvPacket          = "/ibc.core.channel.v1.MsgRecvPacket"
+	MsgAcknowledgement     = "/ibc.core.channel.v1.MsgAcknowledgement"
+
+	MsgTimeoutRefresh = "/ibc.core.channel.v1.MsgChannelCloseInit" //todo: find a better solution for this msg
+	MsgTimeOut        = "/ibc.core.channel.v1.MsgTimeout"
 )
