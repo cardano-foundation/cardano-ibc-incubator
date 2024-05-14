348bb6ec4e6b1032cd836d48bfc7a4b4d2a8fa87
Duc Chinh Nguyen <chinhnd@smartosc.com>
2024-05-14 10:59:50 +0700

    feat: Define client store, keys and encoding for Mithril client

--- cosmos/sidechain/x/clients/mithril/keys.go
+++ cosmos/sidechain/x/clients/mithril/keys.go
@@ -1,5 +1,45 @@
 package mithril
 
-const (
-	ModuleName = "2000-cardano-mithril"
+import (
+	"fmt"
 )
+
+const (
+	ModuleName                           = "2000-cardano-mithril"
+	KeyFirstCertificateMsdInEpochPrefix  = "fcMsdInEpoch"
+	KeyFirstCertificateTsInEpochPrefix   = "fcTsInEpoch"
+	KeyLatestCertificateMsdInEpochPrefix = "LcMsdInEpoch"
+	KeyLatestCertificateTsInEpochPrefix  = "LcTsInEpoch"
+)
+
+func FcMsdInEpochKey(epoch uint64) []byte {
+	return []byte(FcMsdInEpochPath(epoch))
+}
+
+func FcMsdInEpochPath(epoch uint64) string {
+	return fmt.Sprintf("%s/%v", KeyFirstCertificateMsdInEpochPrefix, epoch)
+}
+
+func FcTsInEpochKey(epoch uint64) []byte {
+	return []byte(FcTsInEpochPath(epoch))
+}
+
+func FcTsInEpochPath(epoch uint64) string {
+	return fmt.Sprintf("%s/%v", KeyFirstCertificateTsInEpochPrefix, epoch)
+}
+
+func LcMsdInEpochKey(epoch uint64) []byte {
+	return []byte(LcMsdInEpochPath(epoch))
+}
+
+func LcMsdInEpochPath(epoch uint64) string {
+	return fmt.Sprintf("%s/%v", KeyLatestCertificateMsdInEpochPrefix, epoch)
+}
+
+func LcTsInEpochKey(epoch uint64) []byte {
+	return []byte(LcTsInEpochPath(epoch))
+}
+
+func LcTsInEpochPath(epoch uint64) string {
+	return fmt.Sprintf("%s/%v", KeyLatestCertificateTsInEpochPrefix, epoch)
+}
