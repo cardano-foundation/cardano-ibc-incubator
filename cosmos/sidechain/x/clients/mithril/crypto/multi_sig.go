e9ba01d93dddb4d2d97a6320cf011cff6618c303
datlt <datlt@smartosc.com>
2024-05-15 12:37:55 +0700

    feat(SigningKey): add GenerateSecretKey, Sign, ToBytes and FromBytes functions

--- cosmos/sidechain/x/clients/mithril/crypto/multi_sig.go
+++ cosmos/sidechain/x/clients/mithril/crypto/multi_sig.go
@@ -1,23 +1,28 @@
 package crypto
 
 import (
+	"crypto/rand"
+	"errors"
+
 	blst "github.com/supranational/blst/bindings/go"
 )
 
-type PublicKey = blst.P1Affine
+type BlstVk = blst.P1Affine
 
-type Signature = blst.P2Affine
+type BlstSig = blst.P2Affine
+
+type BlstSk = blst.SecretKey
 
 // / MultiSig secret key, which is a wrapper over the BlstSk type from the blst
 // / library.
 type SigningKey struct {
-	blst.SecretKey
+	*BlstSk
 }
 
 // / MultiSig verification key, which is a wrapper over the BlstVk (element in G2)
 // / from the blst library.
 type VerificationKey struct {
-	PublicKey
+	*BlstVk
 }
 
 // / MultiSig proof of possession, which contains two elements from G1. However,
@@ -26,12 +31,61 @@ type VerificationKey struct {
 // / the other hand, `k2` is a G1 point, as it does not share structure with
 // / the BLS signature, and we need to have an ad-hoc verification mechanism.
 type ProofOfPossession struct {
-	K1 Signature
-	K2 blst.P1
+	K1 *BlstSig
+	K2 *blst.P1
 }
 
 // / MultiSig public key, contains the verification key and the proof of possession.
 type VerificationKeyPoP struct {
-	VK  VerificationKey
-	POP ProofOfPossession
+	/// The verification key.
+	VK *VerificationKey
+	/// Proof of Possession.
+	POP *ProofOfPossession
+}
+
+type Signature struct {
+	*BlstSig
+}
+
+// / ====================== SigningKey implementation ======================
+// / Generate a secret key
+func Gen() (*SigningKey, error) {
+	ikm := make([]byte, 32)
+	_, err := rand.Read(ikm)
+	if err != nil {
+		return nil, err
+	}
+	return &SigningKey{
+		BlstSk: blst.KeyGen(ikm),
+	}, nil
+}
+
+// / Sign a message with the given secret key
+func (sk *SigningKey) Sign(msg []byte) *Signature {
+	var dst = []byte("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
+	sig := new(BlstSig).Sign(sk.BlstSk, msg, dst)
+	return &Signature{
+		BlstSig: sig,
+	}
+}
+
+// / Convert the secret key into byte string.
+func (sk *SigningKey) ToBytes() [32]byte {
+	return [32]byte(sk.BlstSk.Serialize())
+}
+
+// / Convert a string of bytes into a `SigningKey`.
+// /
+// / # Error
+// / Fails if the byte string represents a scalar larger than the group order.
+func (sk *SigningKey) FromBytes(bytes []byte) (*SigningKey, error) {
+	if len(bytes) < 32 {
+		return nil, errors.New("input bytes too short, expected at least 32 bytes")
+	}
+
+	sk.BlstSk = new(BlstSk).Deserialize(bytes[:32])
+	if sk.BlstSk == nil {
+		return nil, errors.New("invalid signing key bytes")
+	}
+	return sk, nil
 }
