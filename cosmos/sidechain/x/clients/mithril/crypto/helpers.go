00adb1ee49873f80ab45cc234e7a5968558a3c15
Duc Chinh Nguyen <chinhnd@smartosc.com>
2024-05-16 19:26:19 +0700

    feat(stm): implement more unsafe Rust helper

--- cosmos/sidechain/x/clients/mithril/crypto/helpers.go
+++ cosmos/sidechain/x/clients/mithril/crypto/helpers.go
@@ -11,7 +11,15 @@ import (
 // ---------------------------------------------------------------------
 // verifyPairing checks if the pairing `e(g1,mvk) = e(k2,g2)` holds.
 func verifyPairing(vk *VerificationKey, pop *ProofOfPossession) bool {
-	return true
+	g1P := blst.P1Generator().ToAffine()
+	mvkP := new(blst.P2Affine).Uncompress(vk.BlstVk.Compress())
+	mlLhs := blst.Fp12MillerLoop(mvkP, g1P)
+
+	k2P := pop.K2.ToAffine()
+	g2P := blst.P2Generator().ToAffine()
+	mlRhs := blst.Fp12MillerLoop(g2P, k2P)
+
+	return blst.Fp12FinalVerify(mlLhs, mlRhs)
 }
 
 func compressP1(k2 *blst.P1) []byte {
@@ -31,9 +39,11 @@ func uncompressP1(bytes []byte) (*blst.P1, error) {
 	return out, nil
 }
 
-func scalarToPkInG1(sk *SigningKey) *blst.P1 {
-	// TO-DO: not implemented
-	return nil
+func scalarToPkInG1(sk *SigningKey) blst.P1 {
+	out := blst.P1{}
+	defaultBlstP1Affine := blst.P1Affine{}
+	out.FromAffine(defaultBlstP1Affine.From(sk.BlstSk))
+	return out
 }
 
 func vkFromP2Affine(vk *VerificationKey) *blst.P2 {
@@ -42,6 +52,21 @@ func vkFromP2Affine(vk *VerificationKey) *blst.P2 {
 	return projectiveP2
 }
 
-func sigToP1(sig *BlstSig) *blst.P1 {
-	return nil
+func sigToP1(sig *BlstSig) blst.P1 {
+	projectiveP1 := blst.P1{}
+	p1AffineFromBlstSig := new(blst.P1Affine).Uncompress(sig.Compress())
+	projectiveP1.FromAffine(p1AffineFromBlstSig)
+	return projectiveP1
+}
+
+func p2AffineToVk(groupedVks *blst.P2) BlstVk {
+	affineP2 := groupedVks.ToAffine()
+	blstVk := new(blst.P1Affine).Uncompress(affineP2.Compress())
+	return *blstVk
+}
+
+func p1AffineToSig(groupedSigs *blst.P1) BlstSig {
+	affineP1 := groupedSigs.ToAffine()
+	blstSig := new(blst.P2Affine).Uncompress(affineP1.Compress())
+	return *blstSig
 }
