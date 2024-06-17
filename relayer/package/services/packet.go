9e4bdb3d5ba6b145d2ff84bda623c287fcd67d03
sonhs <sonhs@smartosc.com>
2024-06-17 14:48:25 +0700

    feat: migrate query proof unreceived packets from gateway to relayer

--- relayer/package/services/packet.go
+++ relayer/package/services/packet.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"fmt"
+	pbchannel "github.com/cardano/proto-types/go/github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	"github.com/cardano/relayer/v1/constant"
 	"github.com/cardano/relayer/v1/package/services/helpers"
 	ibc_types "github.com/cardano/relayer/v1/package/services/ibc-types"
@@ -549,6 +550,78 @@ func (gw *Gateway) QueryUnrecvAcks(req *channeltypes.QueryUnreceivedAcksRequest)
 		Height: clienttypes.Height{
 			RevisionNumber: 0,
 			RevisionHeight: 0,
+		},
+	}, nil
+}
+
+func (gw *Gateway) QueryProofUnreceivedPackets(req *pbchannel.QueryProofUnreceivedPacketsRequest) (*pbchannel.QueryProofUnreceivedPacketsResponse, error) {
+	req, err := helpers.ValidQueryProofUnreceivedPackets(req)
+	if err != nil {
+		return nil, err
+	}
+	channelId := strings.Trim(req.ChannelId, "channel-")
+	channelIdNum, err := strconv.ParseInt(channelId, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	chainHandler, err := helpers.GetChainHandler()
+	if err != nil {
+		return nil, err
+	}
+	policyId := chainHandler.Validators.MintChannel.ScriptHash
+
+	prefixTokenName, err := helpers.GenerateTokenName(helpers.AuthToken{
+		PolicyId: chainHandler.HandlerAuthToken.PolicyID,
+		Name:     chainHandler.HandlerAuthToken.Name,
+	}, constant.CHANNEL_TOKEN_PREFIX, channelIdNum)
+	if err != nil {
+		return nil, err
+	}
+	utxos, err := gw.DBService.FindUtxosByPolicyIdAndPrefixTokenName(policyId, prefixTokenName)
+	if err != nil {
+		return nil, err
+	}
+	if len(utxos) == 0 {
+		return nil, fmt.Errorf("no utxos found for policyId %s and prefixTokenName %s", policyId, prefixTokenName)
+	}
+	if utxos[0].Datum == nil {
+
+		return nil, fmt.Errorf("datum is nil")
+	}
+
+	dataString := *utxos[0].Datum
+	channelDatumDecoded, err := ibc_types.DecodeChannelDatumWithPort(dataString[2:])
+	if err != nil {
+		return nil, err
+	}
+
+	stateNum, ok := channelDatumDecoded.State.Channel.State.(cbor.Tag)
+	if !ok {
+		return nil, fmt.Errorf("state is not cbor tag")
+	}
+
+	proof, err := gw.DBService.FindUtxoByPolicyAndTokenNameAndState(
+		policyId,
+		prefixTokenName,
+		channeltypes.State_name[int32(stateNum.Number-constant.CBOR_TAG_MAGIC_NUMBER)],
+		chainHandler.Validators.MintConnection.ScriptHash,
+		chainHandler.Validators.MintChannel.ScriptHash)
+	if err != nil {
+		return nil, err
+	}
+
+	hash := proof.TxHash[2:]
+	cardanoTxProof, err := gw.MithrilService.GetProofOfACardanoTransactionList(hash)
+	if err != nil {
+		return nil, err
+	}
+	connectionProof := cardanoTxProof.CertifiedTransactions[0].Proof
+
+	return &pbchannel.QueryProofUnreceivedPacketsResponse{
+		Proof: []byte(connectionProof),
+		ProofHeight: &clienttypes.Height{
+			RevisionNumber: 0,
+			RevisionHeight: uint64(proof.BlockNo),
 		},
 	}, nil
 }
