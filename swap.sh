35870d02df313c838c38d8c2189c103761d3e6f9
Giovanni Gargiulo <giovanni.gargiulo@cardanofoundation.org>
2025-09-26 11:15:40 +0100

    chore: swap sh fixes

--- swap.sh
+++ swap.sh
@@ -2,6 +2,10 @@
 CROSSCHAIN_SWAPS_ADDRESS=""
 [ -z "$CROSSCHAIN_SWAPS_ADDRESS" ] && echo "crosschain_swaps contract address not specified!" && exit 1
 CARDANO_RECEIVER="247570b8ba7dc725e9ff37e9757b8148b4d5a125958edac2fd4417b8"
+#==================================Define util funcions=======================================
+check_string_empty() {
+  [ -z $1 ] && echo "$2" && exit 1
+}
 #==================================Setup relayer=======================================
 RLY_CONTAINER_NAME="relayer"
 if ! docker ps --format '{{.Names}}' | grep -q "^$RLY_CONTAINER_NAME$"; then
@@ -13,20 +17,23 @@ rly='docker exec -it relayer bin/rly'
 RELAYER_PATH="demo"
 CARDANO_CHAIN_NAME="ibc-0"
 SIDECHAIN_CHAIN_NAME="ibc-1"
-SENT_AMOUNT="12345-9fc33a6ffaa8d1f600c161aa383739d5af37807ed83347cc133521c96d6f636b"
+SENT_AMOUNT="12345-465209195f27c99dfefdcb725e939ad3262339a9b150992b66673be86d6f636b"
 SIDECHAIN_RECEIVER="pfm"
 HERMES_OSMOSIS_NAME="localosmosis"
 HERMES_SIDECHAIN_NAME="sidechain"
 
 cardano_sidechain_conn_id=$($rly config show --json | jq -r --arg path "$RELAYER_PATH" '.paths[$path].src."connection-id"')
+check_string_empty "$cardano_sidechain_conn_id" "Cardano<->Sidechain connection not found. Exiting..."
 
-cardano_sidechain_chann_id=$($rly query connection-channels $CARDANO_CHAIN_NAME $cardano_sidechain_conn_id --reverse --limit 1 | jq -r '.channel_id')
+cardano_sidechain_chann_id=$($rly query connection-channels "$CARDANO_CHAIN_NAME" "$cardano_sidechain_conn_id" --reverse --limit 1 | jq -r '.[0].channel_id')
+check_string_empty "$cardano_sidechain_chann_id" "Cardano->Sidechain channel not found. Exiting..."
 echo "Cardano->Sidechain channel id: $cardano_sidechain_chann_id"
 
-sidechain_cardano_chann_id=$($rly query connection-channels $CARDANO_CHAIN_NAME $cardano_sidechain_conn_id --reverse --limit 1 | jq -r '.counterparty.channel_id')
+sidechain_cardano_chann_id=$($rly query connection-channels "$CARDANO_CHAIN_NAME" "$cardano_sidechain_conn_id" --reverse --limit 1 | jq -r '.counterparty.channel_id')
 echo "Sidechain->Cardano channel id: $sidechain_cardano_chann_id"
 
-sidechain_osmosis_chann_id=$(hermes --json query channels --chain $HERMES_OSMOSIS_NAME --counterparty-chain $HERMES_SIDECHAIN_NAME --show-counterparty | jq -r 'select(.result) | .result[-1].channel_b')
+sidechain_osmosis_chann_id=$(hermes --json query channels --chain "$HERMES_OSMOSIS_NAME" --counterparty-chain "$HERMES_SIDECHAIN_NAME" --show-counterparty | jq -r 'select(.result) | .result[-1].channel_b')
+check_string_empty "$sidechain_osmosis_chann_id" "Sidechain->Osmosis channel not found. Exiting..."
 echo "Sidechain->Osmosis channel id: $sidechain_osmosis_chann_id"
 
 memo=$(
