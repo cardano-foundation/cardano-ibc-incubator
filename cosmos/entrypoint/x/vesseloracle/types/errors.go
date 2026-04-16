81ded9de798911a331800ddac7e72654dfa7091c
floor-licker <kingofthecoast78@gmail.com>
2026-04-16 14:09:06 -0300

    entrypoint: remove legacy vesseloracle transport

--- cosmos/entrypoint/x/vesseloracle/types/errors.go
+++ cosmos/entrypoint/x/vesseloracle/types/errors.go
@@ -8,8 +8,6 @@ import (
 
 // x/vesseloracle module sentinel errors
 var (
-	ErrInvalidSigner        = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrSample               = sdkerrors.Register(ModuleName, 1101, "sample error")
-	ErrInvalidPacketTimeout = sdkerrors.Register(ModuleName, 1500, "invalid packet timeout")
-	ErrInvalidVersion       = sdkerrors.Register(ModuleName, 1501, "invalid version")
+	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrSample        = sdkerrors.Register(ModuleName, 1101, "sample error")
 )
