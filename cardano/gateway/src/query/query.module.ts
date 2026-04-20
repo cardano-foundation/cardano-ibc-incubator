fdffef89ca2ff2cce89c2c561eb98b2c6fa720b7
floor-licker <kingofthecoast78@gmail.com>
2026-04-20 20:16:46 -0300

    cardano: gate gateway readiness on proof availability

--- cardano/gateway/src/query/query.module.ts
+++ cardano/gateway/src/query/query.module.ts
@@ -1,6 +1,7 @@
 import { Logger, Module } from '@nestjs/common';
 import { QueryService } from './services/query.service';
 import { QueryController } from './query.controller';
+import { GatewayReadinessController } from './gateway-readiness.controller';
 import { LucidModule } from '../shared/modules/lucid/lucid.module';
 import { KupoModule } from '../shared/modules/kupo/kupo.module';
 import { HISTORY_SERVICE } from './services/history.service';
@@ -12,16 +13,18 @@ import { MithrilModule } from '../shared/modules/mithril/mithril.module';
 import { DenomTraceService } from './services/denom-trace.service';
 import { HealthModule } from '../health/health.module';
 import { BridgeManifestService } from './services/bridge-manifest.service';
+import { GatewayReadinessService } from './services/gateway-readiness.service';
 import { YaciHistoryService } from './services/yaci-history.service';
 import { IbcTreeCacheService } from '../shared/services/ibc-tree-cache.service';
 
 @Module({
   imports: [LucidModule, KupoModule, MithrilModule, HealthModule],
-  controllers: [QueryController],
+  controllers: [QueryController, GatewayReadinessController],
   providers: [
     QueryService,
     Logger,
     YaciHistoryService,
+    GatewayReadinessService,
     {
       provide: HISTORY_SERVICE,
       useExisting: YaciHistoryService,
